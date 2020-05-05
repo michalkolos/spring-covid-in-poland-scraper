9301dcbf539ff57b0ea6c209fa2832ed100a0073
Michalkolos <cool.kolos@gmail.com>
2020-05-05 12:47:17 +0200

    Project init.

---
+++ src/main/java/com/michalkolos/covidscraper/CovidScraperApplication.java
@@ -0,0 +1,13 @@
+package com.michalkolos.covidscraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidScraperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/michalkolos/covidscraper/CovidScraperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michalkolos.covidscraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidScraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
